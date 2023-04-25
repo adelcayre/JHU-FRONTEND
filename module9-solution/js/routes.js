6c09f3db4cab635f87cd34a925cef21cc9ce1207
Antoine Delcayre <antoine.delcayre@gmail.com>
2023-04-25 12:29:39 -0700

    link fix back to #

--- module9-solution/js/routes.js
+++ module9-solution/js/routes.js
@@ -16,17 +16,17 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
   // Set up UI states
   $stateProvider
     .state ('home', {
-      url:'/JHU-FRONTEND/module9-solution/index.html#/',
+      url:'/JHU-FRONTEND/module9-solution/#/',
       templateUrl:'/JHU-FRONTEND/module9-solution/home.html'
     })
     .state('categories', {
-      url: '/JHU-FRONTEND/module9-solution/index.html#/categories',
+      url: '/JHU-FRONTEND/module9-solution/#/categories',
       templateUrl: '/JHU-FRONTEND/module9-solution/categories.html',
       controller: 'CategoriesController as menu'
     })
 
     .state('items', {
-      url: '/JHU-FRONTEND/module9-solution/index.html#/items/*categoryShortName',
+      url: '/JHU-FRONTEND/module9-solution/#/items/*categoryShortName',
       templateUrl: '/JHU-FRONTEND/module9-solution/items.html',
       controller: 'ItemsController as menu',
       resolve: {
